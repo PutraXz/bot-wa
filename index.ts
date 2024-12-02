a94e22ce811fc63a7f801efce7669797e94b7251
PutraXz <ytprem200100@gmail.com>
2024-12-02 09:23:04 +0700

    change role admins

--- index.ts
+++ index.ts
@@ -28,33 +28,71 @@ async function connectToWhatsApp() {
       console.log("Opened connection");
     }
   });
+  // const getNameDays = (): string =>{
+  //   const options: Intl.DateTimeFormatOptions = {weekday: 'long' , timeZone: 'Asia/Jakarta'};
+  //   const formatter = new Intl.DateTimeFormat('id-ID', options);
+  //   return formatter.format(new Date()); 
+  // }
+  // const date = new Date();
+  // const testDate = new Date("2024-09-23");
+  // const dataArray = JSON.parse(fs.readFileSync('test-data.json', 'utf-8'));
+  // const countNames = dataArray.tarikan.filter(item => item.nama).length;
+  // const getNames = dataArray.tarikan.map(item => item.nama);
+  // let resultNames = [];
+  // getNames.forEach((name, index) => {
+  //   resultNames.push(`${name} ${index + 1}`);
+  // });
 
+  // const diffdays = Math.floor((date.getTime() - testDate.getTime()) / (1000 * 60 * 60 * 24));
+  // const indexDays = Math.floor(diffdays / 10 ) % countNames;
+  // const dateTarikan = testDate.setDate(testDate.getDate() + (indexDays + 1) * 10);
+  // const formatDate = new Date(dateTarikan);
+  // const day = formatDate.getDate().toString().padStart(2, '0');
+  // const month = (formatDate.getMonth() + 1).toString().padStart(2, '0');
+  // const year = formatDate.getFullYear();
+  // const dateAgain = new Date(dateTarikan).toISOString().split('T')[0];
+
+  // console.log(dataArray);
+  // console.log(resultNames.join("\n"));
+  // console.log(countNames);
+  // console.log(indexDays + 1);
+  // console.log(dataArray.tarikan[indexDays].nama);
+  // console.log(dateAgain);
+  // console.log(date);
+  // console.log(testDate);
   sock.ev.on("messages.upsert", async (m) => {
+    const allowedNumbers = [
+      "62895391518953@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "628972365549@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6285261378437@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6282162621163@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6283836002673@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6285762793274@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "62895422059961@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6282365559541@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
+      "6282267749275@s.whatsapp.net"
+    ];    
     const msg = m.messages[0];
     if (msg.key.fromMe) return;
+    const sendNumber = msg.key.participant || msg.key.remoteJid;
+    if (!allowedNumbers.includes(sendNumber)){
+      console.log(`Nomor ${sendNumber} tidak diizinkan mengirim pesan`);
+      return;
+    }
     const isPingCommand = msg.message?.conversation?.startsWith("!ping");
     const isReplyWithPing = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation?.startsWith("!ping");
     const isPingCommandTwo = msg.message?.conversation?.startsWith(".done");
 
-
-    const getNameDays = (): string =>{
-      const options: Intl.DateTimeFormatOptions = {weekday: 'long' , timeZone: 'Asia/Jakarta'};
-      const formatter = new Intl.DateTimeFormat('id-ID', options);
-      return formatter.format(new Date()); 
-    }
-    const date = new Date();
-    const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-    const getNames = data.names.join("\n");
-    console.log(getNames);
-
-    if (isPingCommandTwo) {
-      const groupDataTwo = await sock.groupMetadata(msg.key.remoteJid as string);
-      if (isPingCommandTwo) {
-        await sock.sendMessage(msg.key.remoteJid as string, {
-          text: `UPDATE PEMBAYARAN ${getNameDays()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} \n${getNames}`,
-        });
-      }
-    }
+    
+   
+    // if (isPingCommandTwo) {
+    //   const groupDataTwo = await sock.groupMetadata(msg.key.remoteJid as string);
+    //   if (isPingCommandTwo) {
+    //     await sock.sendMessage(msg.key.remoteJid as string, {
+    //       text: `UPDATE PEMBAYARAN ${getNameDays()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}\nTarikan ke ${indexDays + 1}(${dataArray.tarikan[indexDays].nama}/${day}/${month}/${year})\n${resultNames.join("\n")}`,
+    //     });
+    //   }
+    // }
     if (isPingCommand || isReplyWithPing) {
         const groupData = await sock.groupMetadata(msg.key.remoteJid as string);
         const mentionedJidList = groupData.participants.map((participant) => participant.id);
