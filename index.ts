078a35bd5604556e5b950e79b5fbde6c5bb2b672
PutraXz <ytprem200100@gmail.com>
2024-12-04 19:10:18 +0700

    test

--- index.ts
+++ index.ts
@@ -60,6 +60,15 @@ async function connectToWhatsApp() {
   // console.log(dateAgain);
   // console.log(date);
   // console.log(testDate);
+     
+    // if (isPingCommandTwo) {
+    //   const groupDataTwo = await sock.groupMetadata(msg.key.remoteJid as string);
+    //   if (isPingCommandTwo) {
+    //     await sock.sendMessage(msg.key.remoteJid as string, {
+    //       text: `UPDATE PEMBAYARAN ${getNameDays()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}\nTarikan ke ${indexDays + 1}(${dataArray.tarikan[indexDays].nama}/${day}/${month}/${year})\n${resultNames.join("\n")}`,
+    //     });
+    //   }
+    // }
   sock.ev.on("messages.upsert", async (m) => {
     const allowedNumbers = [
       "62895391518953@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
@@ -71,8 +80,10 @@ async function connectToWhatsApp() {
       "62895422059961@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
       "6282365559541@s.whatsapp.net", // Ganti dengan nomor yang diizinkan
       "6282267749275@s.whatsapp.net"
-    ];    
+    ];
+    
     const msg = m.messages[0];
+    
     if (msg.key.fromMe) return;
     const sendNumber = msg.key.participant || msg.key.remoteJid;
     if (!allowedNumbers.includes(sendNumber)){
@@ -81,44 +92,132 @@ async function connectToWhatsApp() {
     }
     const isPingCommand = msg.message?.conversation?.startsWith("!ping");
     const isReplyWithPing = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation?.startsWith("!ping");
-    const isPingCommandTwo = msg.message?.conversation?.startsWith(".done");
-
-    
-   
-    // if (isPingCommandTwo) {
-    //   const groupDataTwo = await sock.groupMetadata(msg.key.remoteJid as string);
-    //   if (isPingCommandTwo) {
-    //     await sock.sendMessage(msg.key.remoteJid as string, {
-    //       text: `UPDATE PEMBAYARAN ${getNameDays()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}\nTarikan ke ${indexDays + 1}(${dataArray.tarikan[indexDays].nama}/${day}/${month}/${year})\n${resultNames.join("\n")}`,
-    //     });
-    //   }
-    // }
+    const isPingCommandTwo = msg.message?.conversation?.startsWith(".tambah");
+    const isPingCommandThree = msg.message?.conversation?.startsWith(".cari");
     if (isPingCommand || isReplyWithPing) {
-        const groupData = await sock.groupMetadata(msg.key.remoteJid as string);
-        const mentionedJidList = groupData.participants.map((participant) => participant.id);
+      const groupData = await sock.groupMetadata(msg.key.remoteJid as string);
+      const mentionedJidList = groupData.participants.map((participant) => participant.id);
 
-        const quotedSender = msg.message?.extendedTextMessage?.contextInfo?.participant;
-        const quotedMessageId = msg.message?.extendedTextMessage?.contextInfo?.stanzaId;
-        const quotedText = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation;
+      const quotedSender = msg.message?.extendedTextMessage?.contextInfo?.participant;
+      const quotedMessageId = msg.message?.extendedTextMessage?.contextInfo?.stanzaId;
+      const quotedText = msg.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation;
 
-        if (quotedSender && quotedMessageId) {
-            await sock.sendMessage(msg.key.remoteJid as string, {
-                text: `@everyone`,
-                mentions: [quotedSender],
-                contextInfo: {
-                    stanzaId: quotedMessageId,
-                    participant: quotedSender,
-                    quotedMessage: msg.message?.extendedTextMessage?.contextInfo?.quotedMessage, 
-                },
-            });
-        } else if (isPingCommand) {
-            await sock.sendMessage(msg.key.remoteJid as string, {
-                text: "@everyone",
-                mentions: mentionedJidList,
-            });
-        }
-    }
-    
+      if (quotedSender && quotedMessageId) {
+          await sock.sendMessage(msg.key.remoteJid as string, {
+              text: `@everyone`,
+              mentions: [quotedSender],
+              contextInfo: {
+                  stanzaId: quotedMessageId,
+                  participant: quotedSender,
+                  quotedMessage: msg.message?.extendedTextMessage?.contextInfo?.quotedMessage, 
+              },
+          });
+      } else if (isPingCommand) {
+          await sock.sendMessage(msg.key.remoteJid as string, {
+              text: "@everyone",
+              mentions: mentionedJidList,
+          });
+      }
+  }
+  // if (isPingCommandTwo) {
+  //   const groupData = await sock.groupMetadata(msg.key.remoteJid as string);
+  //   const mentionedJidList = groupData.participants.map((participant) => participant.id);
+  //   const senderId = msg.key.participant;
+  //   const textMessage = msg.message?.conversation;
+  //   const dateNow = new Date();
+  //   const splitMessage = textMessage.split(".tambah")[1]?.trim();
+  
+  //   if (!splitMessage) {
+  //     console.log("Nomor tidak ditemukan dalam pesan.");
+  //     return;
+  //   }
+  
+  //   // Membaca file data.json dan memeriksa apakah nomor sudah ada
+  //   fs.readFile('data.json', 'utf8', (err, data) => {
+  //     let dataMessage;
+  //     if (err) {
+  //       console.log('File not found, creating new file.');
+  //       dataMessage = { message: [] };
+  //     } else {
+  //       dataMessage = JSON.parse(data);
+  //     }
+  
+  //     // Mengecek apakah nomor sudah ada dalam dataMessage
+  //     const isNumberExist = dataMessage.message.some((item) => item.number === splitMessage);
+  
+  //     if (isNumberExist) {
+  //       // Nomor sudah ada dalam data
+  //       sock.sendMessage(msg.key.remoteJid as string, {
+  //         text: `Nomor ${splitMessage} sudah ada dalam daftar.`,
+  //       });
+  //     } else {
+  //       // Menambahkan nomor baru ke dalam data
+  //       dataMessage.message.push({
+  //         number: splitMessage,
+  //         sender: senderId,
+  //         tanggal: `${dateNow.getDate()}-${dateNow.getMonth() + 1}-${dateNow.getFullYear()}`,
+  //       });
+  
+  //       fs.writeFile('data.json', JSON.stringify(dataMessage, null, 2), (err) => {
+  //         if (err) {
+  //           console.log('Error writing file:', err);
+  //         } else {
+  //           console.log('Successfully wrote file');
+  //           sock.sendMessage(msg.key.remoteJid as string, {
+  //             text: `Nomor ${splitMessage} berhasil ditambahkan.`,
+  //           });
+  //         }
+  //       });
+  //     }
+  //   });
+  
+  //   console.log(`Pesan dari ${senderId}: ${splitMessage}`);
+  //   }  
+    // if(isPingCommandThree){
+    //   const UripGetContact = require('urip-getcontact');
+    //   const getContact = new UripGetContact("9c2feac41b8d591ff058a3cc45374e844c0059e52e4e07e02adac24f1f574b0e", "ctJZxE6833cd8e526ce1df60a03b0f8da4784249988ea4ebe6a75abf82");
+    //   getContact.checkNumber("0895391518953")
+    //     .then((data) => {
+    //       console.log(data);
+    //     })
+    //     .catch((err) =>{
+    //       console.log(err);
+    //     });
+      // const groupData = await sock.groupMetadata(msg.key.remoteJid as string);
+      // const mentionedJidList = groupData.participants.map((participant) => participant.id);
+
+      // const quotedSender = msg.key.participant;
+      // const quotedMessageId = msg.key.id
+      // const quotedText =msg.message?.extendedTextMessage?.contextInfo?.quotedMessage;
+
+      // const dataMessage = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+      // const getNumber = dataMessage.message.map(item => item.number);
+      // const textMessage = msg.message?.conversation;
+      // const splitMessage = textMessage.split(".cari")[1]?.trim();
+      // const matchedNumber = dataMessage.message.find(n => n.number == splitMessage);
+      //   if(matchedNumber){
+      //     const finalNumber = matchedNumber.sender.split('@')[0];
+      //       await sock.sendMessage(msg.key.remoteJid as string, {
+      //         text: `Nomor ${splitMessage} ada dalam daftar\npengirim: ${finalNumber}\npada tanggal: ${matchedNumber.tanggal}`,
+      //         mentions: [quotedSender],
+      //         contextInfo: {
+      //           stanzaId: quotedMessageId,
+      //           participant: quotedSender,
+      //           quotedMessage: quotedText
+      //         }
+      //       });
+      //   }else{
+      //     await sock.sendMessage(msg.key.remoteJid as string, {
+      //       text: `Nomor ${splitMessage} tidak ada dalam daftar bermasalah`,
+      //       mentions: [quotedSender],
+      //       contextInfo: {
+      //         stanzaId: quotedMessageId,
+      //         participant: quotedSender,
+      //         quotedMessage: quotedText
+      //       }
+      //     });
+      //   }
+    //}
   });
 }
 
